178885cf408d410e1ae7be6634120de330b1f056
svenakela <sven.wesley@gmail.com>
2023-03-25 23:59:59 +0100

    new views

---
+++ src/teams/FinalView.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Logo from './cup.png';
+import Layout from './Layout';
+
+export default () => (
+  <Layout logo={Logo} name="FINAL" />
+);
---
+++ src/teams/TyresöView.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Logo from './Tyresö.png';
+import Layout from './Layout';
+
+export default () => (
+  <Layout logo={Logo} name="Tyresö Hanviken" />
+);
